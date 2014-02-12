217b26fc4458073febc631d0e2f11d739125ecd1
Timothy Bish <tabish121@gmai.com>
2014-02-12 16:29:34 -0500

    Create some initial project structure

---
+++ amqpjms-client/src/test/java/org/fusesource/amqpjms/ClientTest.java
@@ -0,0 +1,14 @@
+package org.fusesource.amqpjms;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClientTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
