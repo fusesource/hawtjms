d7963c671c2180c11a003d0c5f0a2a1a0bb33840
Timothy Bish <tabish121@gmai.com>
2014-02-25 18:13:31 -0500

    Test send of all Message types.

--- amqpjms-client/src/test/java/org/fusesource/amqpjms/JmsMessageProducerTest.java
+++ amqpjms-client/src/test/java/org/fusesource/amqpjms/JmsMessageProducerTest.java
@@ -20,10 +20,14 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import javax.jms.BytesMessage;
+import javax.jms.MapMessage;
 import javax.jms.Message;
 import javax.jms.MessageProducer;
+import javax.jms.ObjectMessage;
 import javax.jms.Queue;
 import javax.jms.Session;
+import javax.jms.StreamMessage;
+import javax.jms.TextMessage;
 
 import org.apache.activemq.broker.jmx.QueueViewMBean;
 import org.fusesource.amqpjms.jms.JmsConnection;
@@ -84,6 +88,85 @@ public class JmsMessageProducerTest extends AmqpTestSupport {
         MessageProducer producer = session.createProducer(queue);
         BytesMessage message = session.createBytesMessage();
         message.writeUTF("TEST");
+        producer.send(message);
+        QueueViewMBean proxy = getProxyToQueue("test.queue");
+        assertEquals(1, proxy.getQueueSize());
+
+        connection.close();
+    }
+
+    @Test(timeout = 60000)
+    public void testSendJMSMapMessage() throws Exception {
+        JmsConnectionFactory factory = new JmsConnectionFactory(getBrokerAmqpConnectionURI());
+        JmsConnection connection = (JmsConnection) factory.createConnection();
+        assertNotNull(connection);
+        connection.start();
+
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        assertNotNull(session);
+        Queue queue = session.createQueue("test.queue");
+        MessageProducer producer = session.createProducer(queue);
+        MapMessage message = session.createMapMessage();
+        message.setBoolean("Boolean", false);
+        message.setString("STRING", "TEST");
+        producer.send(message);
+        QueueViewMBean proxy = getProxyToQueue("test.queue");
+        assertEquals(1, proxy.getQueueSize());
+
+        connection.close();
+    }
+
+    @Test(timeout = 60000)
+    public void testSendJMSStreamMessage() throws Exception {
+        JmsConnectionFactory factory = new JmsConnectionFactory(getBrokerAmqpConnectionURI());
+        JmsConnection connection = (JmsConnection) factory.createConnection();
+        assertNotNull(connection);
+        connection.start();
+
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        assertNotNull(session);
+        Queue queue = session.createQueue("test.queue");
+        MessageProducer producer = session.createProducer(queue);
+        StreamMessage message = session.createStreamMessage();
+        message.writeString("TEST");
+        producer.send(message);
+        QueueViewMBean proxy = getProxyToQueue("test.queue");
+        assertEquals(1, proxy.getQueueSize());
+
+        connection.close();
+    }
+
+    @Test(timeout = 60000)
+    public void testSendJMSTextMessage() throws Exception {
+        JmsConnectionFactory factory = new JmsConnectionFactory(getBrokerAmqpConnectionURI());
+        JmsConnection connection = (JmsConnection) factory.createConnection();
+        assertNotNull(connection);
+        connection.start();
+
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        assertNotNull(session);
+        Queue queue = session.createQueue("test.queue");
+        MessageProducer producer = session.createProducer(queue);
+        TextMessage message = session.createTextMessage("TEST");
+        producer.send(message);
+        QueueViewMBean proxy = getProxyToQueue("test.queue");
+        assertEquals(1, proxy.getQueueSize());
+
+        connection.close();
+    }
+
+    @Test(timeout = 60000)
+    public void testSendJMSObjectMessage() throws Exception {
+        JmsConnectionFactory factory = new JmsConnectionFactory(getBrokerAmqpConnectionURI());
+        JmsConnection connection = (JmsConnection) factory.createConnection();
+        assertNotNull(connection);
+        connection.start();
+
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        assertNotNull(session);
+        Queue queue = session.createQueue("test.queue");
+        MessageProducer producer = session.createProducer(queue);
+        ObjectMessage message = session.createObjectMessage("TEST");
         producer.send(message);
         QueueViewMBean proxy = getProxyToQueue("test.queue");
         assertEquals(1, proxy.getQueueSize());
