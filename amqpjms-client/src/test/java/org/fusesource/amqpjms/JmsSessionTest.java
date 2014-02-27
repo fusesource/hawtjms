57d29e1f29a734ecfd04adcb4cf9f611cdabcb33
Timothy Bish <tabish121@gmai.com>
2014-02-27 14:55:23 -0500

    Start testing session methods for exceptions when called after the connection has failed.

--- amqpjms-client/src/test/java/org/fusesource/amqpjms/JmsSessionTest.java
+++ amqpjms-client/src/test/java/org/fusesource/amqpjms/JmsSessionTest.java
@@ -17,7 +17,16 @@
 package org.fusesource.amqpjms;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.ExceptionListener;
+import javax.jms.JMSException;
 import javax.jms.MessageConsumer;
 import javax.jms.MessageProducer;
 import javax.jms.Queue;
@@ -25,12 +34,17 @@ import javax.jms.Session;
 
 import org.fusesource.amqpjms.jms.JmsConnection;
 import org.fusesource.amqpjms.jms.JmsConnectionFactory;
+import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestName;
 
 /**
  * Test basic Session functionality.
  */
 public class JmsSessionTest extends AmqpTestSupport {
+
+    @Rule public TestName name = new TestName();
 
     @Test(timeout = 60000)
     public void testCreateSession() throws Exception {
@@ -75,6 +89,90 @@ public class JmsSessionTest extends AmqpTestSupport {
 
         consumer.close();
         session.close();
+        connection.close();
+    }
+
+    @Test
+    public void testCreateConsumerThrowsWhenNotConnected() throws Exception {
+        final CountDownLatch latch = new CountDownLatch(1);
+        Connection connection = createAmqpConnection();
+        connection.setExceptionListener(new ExceptionListener() {
+
+            @Override
+            public void onException(JMSException exception) {
+                latch.countDown();
+            }
+        });
+        connection.start();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Destination destination = session.createQueue(name.getMethodName());
+
+        stopBroker();
+        assertTrue(latch.await(10, TimeUnit.SECONDS));
+
+        try {
+            session.createConsumer(destination);
+            fail("Should have thrown once connection failed.");
+        } catch (JMSException ex) {
+            // Expected
+        }
+
+        connection.close();
+    }
+
+    @Test
+    public void testCreateProducerThrowsWhenNotConnected() throws Exception {
+        final CountDownLatch latch = new CountDownLatch(1);
+        Connection connection = createAmqpConnection();
+        connection.setExceptionListener(new ExceptionListener() {
+
+            @Override
+            public void onException(JMSException exception) {
+                latch.countDown();
+            }
+        });
+        connection.start();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Destination destination = session.createQueue(name.getMethodName());
+
+        stopBroker();
+        assertTrue(latch.await(10, TimeUnit.SECONDS));
+
+        try {
+            session.createProducer(destination);
+            fail("Should have thrown once connection failed.");
+        } catch (JMSException ex) {
+            // Expected
+        }
+
+        connection.close();
+    }
+
+    @Ignore
+    @Test
+    public void testCreateTextMessageThrowsWhenNotConnected() throws Exception {
+        final CountDownLatch latch = new CountDownLatch(1);
+        Connection connection = createAmqpConnection();
+        connection.setExceptionListener(new ExceptionListener() {
+
+            @Override
+            public void onException(JMSException exception) {
+                latch.countDown();
+            }
+        });
+        connection.start();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        stopBroker();
+        assertTrue(latch.await(10, TimeUnit.SECONDS));
+
+        try {
+            session.createTextMessage();
+            fail("Should have thrown once connection failed.");
+        } catch (JMSException ex) {
+            // Expected
+        }
+
         connection.close();
     }
 }
