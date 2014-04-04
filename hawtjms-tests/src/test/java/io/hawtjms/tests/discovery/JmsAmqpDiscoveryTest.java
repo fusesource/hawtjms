dee83fe839fcf56d49e97b566ce8528c0bd3bbe3
Timothy Bish <tabish121@gmai.com>
2014-04-04 14:30:44 -0400

    Fix package name.

--- hawtjms-tests/src/test/java/io/hawtjms/tests/discovery/JmsAmqpDiscoveryTest.java
+++ hawtjms-tests/src/test/java/io/hawtjms/tests/discovery/JmsAmqpDiscoveryTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.hawtjms.tests;
+package io.hawtjms.tests.discovery;
 
 import org.junit.Test;
 
