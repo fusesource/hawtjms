30f9b8c5eeb8fecc5c47a842b39071913abfcf4b
Timothy Bish <tabish121@gmai.com>
2014-04-04 17:19:08 -0400

    Move failover tests from the AMQP module to the tests module.

--- hawtjms-tests/src/test/java/io/hawtjms/tests/failover/JmsFailoverTest.java
+++ hawtjms-tests/src/test/java/io/hawtjms/tests/failover/JmsFailoverTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.hawtjms.provider.failover;
+package io.hawtjms.tests.failover;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
