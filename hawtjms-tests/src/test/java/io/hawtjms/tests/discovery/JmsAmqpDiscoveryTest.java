21c7700617e8168051009cf86d5c7bf91f2d4149
Timothy Bish <tabish121@gmai.com>
2014-04-04 14:41:38 -0400

    Add log4j configuration

--- hawtjms-tests/src/test/java/io/hawtjms/tests/discovery/JmsAmqpDiscoveryTest.java
+++ hawtjms-tests/src/test/java/io/hawtjms/tests/discovery/JmsAmqpDiscoveryTest.java
@@ -16,14 +16,61 @@
  */
 package io.hawtjms.tests.discovery;
 
+import static org.junit.Assert.assertTrue;
+import io.hawtjms.jms.JmsConnection;
+import io.hawtjms.jms.JmsConnectionFactory;
+import io.hawtjms.jms.JmsConnectionListener;
+import io.hawtjms.jms.message.JmsInboundMessageDispatch;
+import io.hawtjms.test.util.AmqpTestSupport;
+import io.hawtjms.test.util.Wait;
+
+import javax.jms.Connection;
+
 import org.junit.Test;
 
 /**
  * Test that a Broker using AMQP can be discovered and JMS operations can be performed.
  */
-public class JmsAmqpDiscoveryTest {
+public class JmsAmqpDiscoveryTest extends AmqpTestSupport implements JmsConnectionListener {
 
     @Test
-    public void test() {
+    public void testRunningBrokerIsDiscovered() throws Exception {
+        connection = createConnection();
+        final JmsConnection jmsConnection = (JmsConnection) connection;
+        connection.start();
+
+        assertTrue("connection never connected.", Wait.waitFor(new Wait.Condition() {
+
+            @Override
+            public boolean isSatisified() throws Exception {
+                return jmsConnection.isConnected();
+            }
+        }));
+    }
+
+    @Override
+    protected boolean isAmqpDiscovery() {
+        return true;
+    }
+
+    protected Connection createConnection() throws Exception {
+        JmsConnectionFactory factory = new JmsConnectionFactory("discovery:multicast://default");
+        return factory.createConnection();
+    }
+
+    @Override
+    public void onConnectionFailure(Throwable error) {
+    }
+
+    @Override
+    public void onConnectionInterrupted() {
+    }
+
+    @Override
+    public void onConnectionRestored() {
+    }
+
+    @Override
+    public void onMessage(JmsInboundMessageDispatch envelope) {
     }
 }
